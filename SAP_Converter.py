e32c9307e3a35d53d3574fb04728ca8edc32afee
Mohamed Alshabrawy <Mohamed.Alshabrawy@logrhythm.com>
2019-08-27 01:50:24 +0300

    Updated after testing

--- SAP_Converter.py
+++ SAP_Converter.py
@@ -5,14 +5,13 @@ from textwrap import wrap
 
 encoded = "utf-16-le" #unicode, utf-16, utf-16-le, utf-16-be, utf-8
 
-SrcPath = "D:\\LogRhythm\\MPE\\SAP\\SIEM\\"
+SrcPath = "/usr/sap/dev/log" #Audit log files path
 
-AUDFiles = Path(SrcPath).glob("*.AUD")
+AUDFiles = Path(SrcPath).glob("audit*") #Get all files with thename audit
 
 for file in AUDFiles:
-    #print(file)
     sf = open(file,mode="r", encoding=encoded)
-    df = open(str(file).split('.AUD')[0]+'.log',"w+")
+    df = open(str(file).split('.log')[0]+'.aud',"w+")
     content = sf.read()
     for i in range(0,len(content),200):
         df.write(content[i:i+200]+"\n")
---
+++ SAP_Converter_Daily.py
@@ -0,0 +1,44 @@
+import io
+import os
+import datetime
+from textwrap import wrap
+
+encoded = "utf-16-le" #unicode, utf-16, utf-16-le, utf-16-be, utf-8
+
+srcPath = "/home/oc" #Audit log files path
+#srcPath = "D:\\LogRhythm\\MPE\\SAP\\SIEM"
+audFile = srcPath + "/audit_" + datetime.datetime.now().strftime("%Y%m%d") + ".log"
+
+stateFile = srcPath + "/audit.stat"
+#stateFile = srcPath + "\\audit.stat"
+if os.path.exists(audFile):
+    audSize = os.path.getsize(audFile)
+    if os.path.exists(stateFile):
+        sf = open(stateFile,"r+")
+        stSize = int(sf.read().strip())
+        if audSize > stSize:
+            af = open(AUDfile,mode="r", encoding=encoded)
+            lf = open(str(AUDfile).split('.log')[0]+'.aud',"w+")
+            content = af.read()
+            for i in range(0,len(content),200):
+                lf.write(content[i:i+200]+"\n")
+            sf.write(stSize)
+            sf.close()
+            af.close()
+            lf.close()
+        quit(0)
+    else:
+        sf = open(stateFile,"w+")
+        af = open(audFile,mode="r", encoding=encoded)
+        lf = open(str(audFile).split('.log')[0]+'.aud',"w+")
+        content = af.read()
+        for i in range(0,len(content),200):
+            lf.write(content[i:i+200]+"\n")
+        sf.write(str(audSize))
+        sf.close()
+        af.close()
+        lf.close()
+        quit(0)
+    
+else:
+    quit(0)
